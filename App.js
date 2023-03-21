84cafb9de2b7f3decc40a6da252bedb29953a6bb
lethanhtingl <ltt.lethanhtin@gmail.com>
2023-03-22 01:02:36 +0700

    chatListItem-ui

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './src/components/chatlistitem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ChatListItem />
+      <ChatListItem />
     </View>
   );
 }
